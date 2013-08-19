bd9bb05864655fb89912256f2e0cd9573b310ee2
Lex Robinson <lexi@lexi.org.uk>
2013-08-19 16:10:47 +0100

    Added chat commands to shitbot

--- shitbot.js
+++ shitbot.js
@@ -14,11 +14,53 @@
 	if ( window.shitbot )
 		window.shitbot.seppuku();
 	var shitbot = window.shitbot = {
+		version: '0.3',
+
 		currentTrack: null,
 
 		me: null,
 
 		chats: 2, // you aint been spamming yet baby
+
+		commands: {
+			'die': function( userName, message, userID )
+			{
+				var user = plug.getUser( userID );
+				if ( user.permission >= plug.ROLE.BOUNCER )
+					this.seppuku();
+				else
+					this.doChat( 'YOU CAN\'T TELL ME WHAT TO DO @', userName.toUpperCase(), '!' );
+			},
+			'about': function( userName )
+			{
+				this.doChat( '@', userName, ': This is shitbot v', this.version, ' filling in the gaps until a better bot comes online.' );
+			},
+			'status': function()
+			{
+				this.doChat( 'Not great' );
+			},
+			'taco': function( userName )
+			{
+				this.doChat( '@', userName, ': Do I look like a chef?' );
+			},
+			'drink': function( userName )
+			{
+				this.doChat( '@', userName, ': Do I look like a barkeep?' );
+			},
+			// woop
+			'info': function()
+			{
+				this.doChat( "No Dubstep or Skrillex! 8 minutes max each tune. We play DnB like Liquidfunk, Neurofunk, Jungle, Jump up or *Breaks* except Drumstep and Breakcore. Read about the genre we play here: http://en.wikipedia.org/wiki/Drum_and_bass" );
+			},
+			'rules': function()
+			{
+				this.doChat( "The only genres allowed here are DnB and Breakbeat except Drumstep and Breakcore. If a tune/mix is off-genre or involves Skrillex, it will be skipped. If a tune/mix is longer than 8 minutes, it will be skipped at the 8-minute mark. Also, have fun!" );
+			},
+			'whymeh': function()
+			{
+				this.doChat( "Please reserve Mehs for songs that are a) extremely overplayed b) off genre c) absolutely god awful or d) troll songs. If you simply aren't feeling a song, then please remain neutral." );
+			}
+		},
 
 		doChat: function()
 		{
@@ -61,6 +103,22 @@
 				this.chats = 0;
 			else
 				this.chats++;
+			if ( ! this.chats || 'message' !== data.type )
+				return;
+			var message = data.message;
+			if ( '.' === message ) {
+				this.doChat( '.' );
+			}
+			if ( ';' !== message.substring(0, 1) )
+				return;
+			var spaceLoc = message.indexOf(' ');
+			if ( -1 === spaceLoc )
+				spaceLoc = undefined;
+			var cmd = message.substring( 1, spaceLoc );
+			var command = this.commands[ cmd ];
+			if ( command ) {
+				command.call( this, data.from, spaceLoc ? message.substring( spaceLoc + 1 ) : '', data.fromID );
+			}
 		},
 
 		onLoad: function()
@@ -71,14 +129,17 @@
 			plug.on( plug.CURATE_UPDATE, this.onCurate, this );
 			plug.on( plug.DJ_ADVANCE, this.onAdvance, this );
 			plug.on( plug.CHAT, this.onChat, this );
+			this.doChat( 'Shitbot v', this.version, ' now online!' );
 		},
 
 		seppuku: function()
 		{
+			console.log( 'RIP' );
 			plug.off( plug.ROOM_SCORE_UPDATE, this.onRoomScoreUpdate, this );
 			plug.off( plug.CURATE_UPDATE, this.onCurate, this );
 			plug.off( plug.DJ_AVDANCE, this.onAdvance, this );
 			plug.off( plug.CHAT, this.onChat, this );
+			this.doChat( 'Shitbot v', this.version, ' signing off. Bye!' );
 		}
 	};
 	_.bindAll( shitbot, 'onLoad' );
